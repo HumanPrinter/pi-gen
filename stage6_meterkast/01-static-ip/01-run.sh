53326e085c1e727d81c05cb88cc366520e4c5539
Oscar Brouwer <HumanPrinter@users.noreply.github.com>
2025-06-17 08:21:04 +0200

    Add vlan support

--- stage6_meterkast/01-static-ip/01-run.sh
+++ stage6_meterkast/01-static-ip/01-run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+echo '8021q' >> ${ROOTFS_DIR}/etc/modules
+
 mkdir -p ${ROOTFS_DIR}/etc/netplan/
 
 cat > ${ROOTFS_DIR}/etc/netplan/eth0.yaml <<EOF
@@ -17,6 +19,12 @@ network:
       nameservers:
         addresses:
         - 192.168.100.1
+  vlans:
+    vlan200:
+      id: 200
+      link: eth0
+      addresses:
+      - 192.168.200.252/24
 EOF
 
 chmod 600 ${ROOTFS_DIR}/etc/netplan/eth0.yaml
